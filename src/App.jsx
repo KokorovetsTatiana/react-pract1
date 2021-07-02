c7cd7372cbeed9a79ceaa1e494db20e718b26758
KokorovetsTatiana <tanuska@ukr.net>
2021-07-02 20:48:27 +0300

    сформировала пакет

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css';
+import Wrapper from './components/Wrapper/Wrapper'
+
+function App() {
+  return <div className="App">
+    <Wrapper></Wrapper>
+  </div>
+}
+
+export default App;
